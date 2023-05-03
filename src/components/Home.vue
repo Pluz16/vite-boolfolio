fe59679e260f69684bba719386c37023a0c55c81
Francesco Punta <puntafrancesco@gmail.com>
2023-05-03 15:55:40 +0200

    Create router and Home

---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <h1>Benvenuti nella mia pagina personale</h1>
+      <p>Qui potete trovare informazioni su di me e sui miei progetti</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Home'
+  }
+  </script>
+  
+  <style scoped>
+  .home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f9f9f9;
+  color: #333;
+  font-size: 2rem;
+  text-align: center;
+}
+
+  </style>
+  
