4363bfc03979b95a72b3a8447c606c1d45e8caf1
Francesco Punta <puntafrancesco@gmail.com>
2023-05-04 15:54:21 +0200

    Layout di base

---
+++ src/components/BaseLayout.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+      <nav>
+        <a href="#">Logo</a>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Portfolio</a></li>
+        </ul>
+      </nav>
+      
+      <div class="container">
+        <slot name="content" />
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Layout',
+    props: {
+      pageTitle: {
+        type: String,
+        default: 'My Website'
+      }
+    },
+  };
+  </script>
+
+  <style lang="scss">
+$primary-color: #007bff;
+$secondary-color: #6c757d;
+$nav-height: 4rem;
+
+nav {
+  background-color: $primary-color;
+  height: $nav-height;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 1rem;
+  
+  a {
+    color: white;
+    text-decoration: none;
+    font-size: 1.2rem;
+    font-weight: bold;
+    text-transform: uppercase;
+  }
+}
+
+.main-container {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 1rem;
+}
+
+
+
+    </style>
