7cba327a39e316cb6d88fa3e9f8143ff457054e6
Francesco Punta <puntafrancesco@gmail.com>
2023-05-07 18:36:08 +0200

    Bonus

--- src/pages/ProjectDetail.vue
+++ src/pages/ProjectDetail.vue
@@ -1,49 +1,70 @@
 <template>
-    <div class="container py-5">
-      <div class="row">
-        <div class="col-12">
-          <h1 class="mb-5">{{ project.title }}</h1>
-        </div>
+  <div class="container py-5">
+    <div class="row">
+      <div class="col-12">
+        <h1 class="mb-5">{{ project.title }}</h1>
       </div>
-      <div class="row">
-        <div class="col-12 col-md-6">
-          <img :src="project.image" class="img-fluid mb-3" alt="">
-        </div>
-        <div class="col-12 col-md-6">
-          <p>{{ project.description }}</p>
+    </div>
+    <div class="row">
+      <div class="col-12 col-md-6">
+        <img :src="project.image" class="img-fluid mb-3" alt="">
+      </div>
+      <div class="col-12 col-md-6">
+        <p>{{ project.description }}</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <h2 class="mb-3">Altri progetti correlati:</h2>
+        <div class="row">
+          <project-card v-for="project in relatedProjects" :key="project.id" :project="project">
+            <router-link :to="{ name: 'projectDetail', params: { id: project.id }}">Vedi dettagli</router-link>
+          </project-card>
         </div>
       </div>
     </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    name: 'ProjectDetail',
-    data() {
-      return {
-        project: {},
-      };
+  </div>
+</template>
+<script>
+import axios from 'axios';
+import ProjectCard from './ProjectCard.vue';
+
+export default {
+  name: 'ProjectDetail',
+  components: {
+    ProjectCard,
+  },
+  data() {
+    return {
+      project: {},
+      relatedProjects: [],
+    };
+  },
+  async mounted() {
+    await this.fetchProject();
+    await this.fetchRelatedProjects();
+  },
+  methods: {
+    async fetchProject() {
+      try {
+        const response = await axios.get(`http://localhost:8000/api/projects/${this.$route.params.id}`);
+        this.project = response.data.data;
+        console.log(this.project);
+      } catch (error) {
+        console.error(error);
+      }
     },
-    async mounted() {
-      await this.fetchProject();
+    async fetchRelatedProjects() {
+      try {
+        const response = await axios.get(`http://localhost:8000/api/projects?category=${this.project.category}&_limit=3&_sort=createdAt:desc`);
+        this.relatedProjects = response.data.data;
+        console.log(this.relatedProjects);
+      } catch (error) {
+        console.error(error);
+      }
     },
-    methods: {
-      async fetchProject() {
-        try {
-          const response = await axios.get(`http://localhost:8000/api/projects/${this.$route.params.id}`);
-          this.project = response.data.data;
-          console.log(this.project);
-        } catch (error) {
-          console.error(error);
-        }
-      },
-    },
-  };
-  </script>
-  
-  <style scoped>
-  </style>
-  
-  
+  },
+};
+</script>
+<style scoped>
+</style>
